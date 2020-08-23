f04690868bf55f322c09a389b07485740b4ac7de
Rajkumar Chandrasekaran <rajkumar.chandrasekaran@cognizant.com>
2020-08-23 15:08:11 +0530

    types of versioning

--- src/main/java/com/example/weebservices/springboot/versioning/PersonController.java
+++ src/main/java/com/example/weebservices/springboot/versioning/PersonController.java
@@ -6,13 +6,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
     
-    @GetMapping(path = "/person/v1")
+    @GetMapping(path = "/person/param",params = "version=1")
     public Person getPerson(){
         return new Person("Rajkumar Chandrasekaran");
     }
 
-    @GetMapping(path = "/person/v2")
+    @GetMapping(path = "/person/param", params = "version=2")
    public PersonV2 getPersonV2(){
+       return new PersonV2(new Name("Rajkumar", "Chandrasekaran"));
+   }
+
+
+   @GetMapping(path = "/person/header",headers = "X-API-VERSION=1")
+    public Person getheaderPerson(){
+        return new Person("Rajkumar Chandrasekaran");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+   public PersonV2 getheaderPersonV2(){
+       return new PersonV2(new Name("Rajkumar", "Chandrasekaran"));
+   }
+
+   @GetMapping(path = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public Person getProducePersonV1(){
+        return new Person("Rajkumar Chandrasekaran");
+    }
+
+    @GetMapping(path = "/person/produces", produces = "application/vnd.company.app-v2+json")
+   public PersonV2 getProducePersonV2(){
        return new PersonV2(new Name("Rajkumar", "Chandrasekaran"));
    }
 
