0d6e3b3f400d76b6ef636280012cc4ba6d1aea80
Rajkumar Chandrasekaran <rajkumar.chandrasekaran@cognizant.com>
2020-08-23 15:22:58 +0530

    basic security level changes

--- src/main/java/com/example/weebservices/springboot/versioning/PersonController.java
+++ src/main/java/com/example/weebservices/springboot/versioning/PersonController.java
@@ -5,36 +5,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
-    
-    @GetMapping(path = "/person/param",params = "version=1")
-    public Person getPerson(){
+
+    @GetMapping(path = "/person/param", params = "version=1")
+    public Person getPerson() {
         return new Person("Rajkumar Chandrasekaran");
     }
 
     @GetMapping(path = "/person/param", params = "version=2")
-   public PersonV2 getPersonV2(){
-       return new PersonV2(new Name("Rajkumar", "Chandrasekaran"));
-   }
+    public PersonV2 getPersonV2() {
+        return new PersonV2(new Name("Rajkumar", "Chandrasekaran"));
+    }
 
-
-   @GetMapping(path = "/person/header",headers = "X-API-VERSION=1")
-    public Person getheaderPerson(){
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public Person getheaderPerson() {
         return new Person("Rajkumar Chandrasekaran");
     }
 
     @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
-   public PersonV2 getheaderPersonV2(){
-       return new PersonV2(new Name("Rajkumar", "Chandrasekaran"));
-   }
+    public PersonV2 getheaderPersonV2() {
+        return new PersonV2(new Name("Rajkumar", "Chandrasekaran"));
+    }
 
-   @GetMapping(path = "/person/produces", produces = "application/vnd.company.app-v1+json")
-    public Person getProducePersonV1(){
+    @GetMapping(path = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public Person getProducePersonV1() {
         return new Person("Rajkumar Chandrasekaran");
     }
 
     @GetMapping(path = "/person/produces", produces = "application/vnd.company.app-v2+json")
-   public PersonV2 getProducePersonV2(){
-       return new PersonV2(new Name("Rajkumar", "Chandrasekaran"));
-   }
+    public PersonV2 getProducePersonV2() {
+        return new PersonV2(new Name("Rajkumar", "Chandrasekaran"));
+    }
 
 }
